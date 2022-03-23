274a333da2b6f7b3c385bebc4dcab5cc4a013e4c
JPAlkamim <jpalkamim@gmail.com>
2022-03-23 10:52:32 -0300

    add function nearest neighbor

--- travelingSalesman.py
+++ travelingSalesman.py
@@ -1,9 +1,11 @@
 import math
 import random
 
+
 class Matrix:
     def __init__(self, n, m):
         self.matrix = self.getMatrix(n, m)
+        self.n = n
 
     def getMatrix(self, n, m):
         num = math.inf
@@ -11,58 +13,74 @@ class Matrix:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 matrix[i][j] = num
-        return matrix 
-    
+        return matrix
 
     def getElement(self, i, j):
         return self.matrix[i-1][j-1]
-    
+
     def setElement(self, i, j, element):
         self.matrix[i-1][j-1] = element
-    
+
 
 class Vertex:
-    def __init__ (self, id, x, y):
+    def __init__(self, id, x, y):
         self.id = int(id)
         self.x = int(x)
         self.y = int(y)
-    
-def weightEdge(v1,v2):
-    return math.sqrt((v1.x-v2.x)**2 + (v1.y-v2.y)**2) 
 
-listVertex = []
-contLines = 0
-file = open('pontos.txt', 'r')
-for line in file:
-    listVertex.append(Vertex(contLines + 1, line.split()[1], line.split()[-1])) 
-    contLines += 1
 
-graph = Matrix(contLines, contLines)
+def weightEdge(v1, v2):
+    return math.sqrt((v1.x-v2.x)**2 + (v1.y-v2.y)**2)
 
-for i in range(len(listVertex)):
-    for j in range(len(listVertex)):
-        graph.setElement(i, j, weightEdge(listVertex[i], listVertex[j]))
 
-randomVertex = random.randint(0, contLines - 1)
+def makeMatrix():
+    listVertex = []
+    contLines = 0
+    file = open('pontos.txt', 'r')
+    for line in file:
+        listVertex.append(
+            Vertex(contLines + 1, line.split()[1], line.split()[-1]))
+        contLines += 1
 
-listVisit = []
-listVisit.append(randomVertex)
-edges = []
+    graph = Matrix(contLines, contLines)
 
-while len(listVisit) < contLines:
-    min = math.inf
-    for j in range(len(listVertex)):
-        if j not in listVisit:
-            if graph.getElement(randomVertex, j) < min:
-                min = graph.getElement(randomVertex, j)
-                i = j
-    listVisit.append(i)
-    edges.append([(randomVertex, i), min])
-    randomVertex = i
+    for i in range(len(listVertex)):
+        for j in range(len(listVertex)):
+            graph.setElement(i, j, weightEdge(listVertex[i], listVertex[j]))
 
-for element in edges:
-    print(element)
+    return graph
 
-betterWeight = [item[1] for item in edges]
 
-print(sum(betterWeight))
+def nearestNeighbor(graph):
+    randomVertex = random.randint(0, graph.n)
+
+    listVisit = []
+    listVisit.append(randomVertex)
+    edges = []
+
+    while len(listVisit) < graph.n:
+        min = math.inf
+        for j in range(graph.n):
+            if j not in listVisit:
+                if graph.getElement(randomVertex, j) < min:
+                    min = graph.getElement(randomVertex, j)
+                    i = j
+        listVisit.append(i)
+        edges.append([(randomVertex, i), min])
+        randomVertex = i
+
+    for element in edges:
+        print(element)
+
+    betterWeight = [item[1] for item in edges]
+
+    print(sum(betterWeight))
+
+
+def main():
+    graph = makeMatrix()
+    nearestNeighbor(graph)
+
+
+if __name__ == '__main__':
+    main()
