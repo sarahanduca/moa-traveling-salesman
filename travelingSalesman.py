0c61c6ca4f8628918305bd48b18bff41643e7d2a
JpAlkamim <ra112648@uem.br>
2022-03-28 12:00:28 -0300

    added input

--- travelingSalesman.py
+++ travelingSalesman.py
@@ -1,5 +1,6 @@
 import math
 import random
+import re
 
 
 class Matrix:
@@ -25,19 +26,51 @@ class Matrix:
 class Vertex:
     def __init__(self, id, x, y):
         self.id = int(id)
-        self.x = int(x)
-        self.y = int(y)
+        if self.isDecimal(x):
+            self.x = int(x)
+            self.y = int(y)
+        else:
+            self.x = float(x)
+            self.y = float(y)
+
+    def isDecimal(self, x):
+        return re.findall(".", x)
 
 
 def weightEdge(v1, v2):
-    return math.sqrt((v1.x-v2.x)**2 + (v1.y-v2.y)**2)
+    return math.sqrt(((v1.x-v2.x)**2) + ((v1.y-v2.y)**2))
 
 
-def makeMatrix():
+# def makeMatrix():
+#     listVertex = []
+#     contLines = 0
+#     file = open('pontos.txt', 'r')
+#     for line in file:
+#         listVertex.append(
+#             Vertex(contLines + 1, line.split()[1], line.split()[-1]))
+#         contLines += 1
+
+#     graph = Matrix(contLines, contLines)
+
+#     for i in range(len(listVertex)):
+#         for j in range(len(listVertex)):
+#             graph.setElement(i, j, weightEdge(listVertex[i], listVertex[j]))
+
+#     return graph
+
+
+def readInput():
+    description = []
     listVertex = []
+    enter = ''
     contLines = 0
-    file = open('pontos.txt', 'r')
-    for line in file:
+
+    while enter != 'EOF':
+        enter = input()
+        description.append(enter.strip('\r'))
+    i = description.index('NODE_COORD_SECTION')
+    constructor = description[i + 1: len(description) - 1]
+    for line in constructor:
         listVertex.append(
             Vertex(contLines + 1, line.split()[1], line.split()[-1]))
         contLines += 1
@@ -53,6 +86,7 @@ def makeMatrix():
 
 def nearestNeighbor(graph):
     randomVertex = random.randint(0, graph.n)
+    firstVertex = randomVertex
 
     listVisit = []
     listVisit.append(randomVertex)
@@ -74,7 +108,7 @@ def nearestNeighbor(graph):
 
     betterWeight = [item[1] for item in edges]
 
-    print(sum(betterWeight))
+    return sum(betterWeight)
 
 
 def farestNeighbor(graph):
@@ -100,13 +134,15 @@ def farestNeighbor(graph):
 
     betterWeight = [item[1] for item in edges]
 
-    print(sum(betterWeight))
+    return sum(betterWeight)
 
 
 def main():
-    graph = makeMatrix()
-    farestNeighbor(graph)
+    graph = readInput()
+    result = nearestNeighbor(graph)
+
+    return result
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
